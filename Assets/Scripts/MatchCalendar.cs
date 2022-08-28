c2ab8735c89a6f739997b133c80c91bda09744f8
tomekszub <szuberttomek0@gmail.com>
2022-08-28 20:52:44 +0200

    removed unused usings

--- Assets/Scripts/MatchCalendar.cs
+++ Assets/Scripts/MatchCalendar.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 public static class MatchCalendar
 {
