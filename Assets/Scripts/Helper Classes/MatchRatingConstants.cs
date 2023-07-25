a59cea2b4bf6168d79ef6705d1a96c970b7aada3
tomekszub <szuberttomek0@gmail.com>
2023-07-25 20:24:06 +0200

    created match rating constants

---
+++ Assets/Scripts/Helper Classes/MatchRatingConstants.cs
@@ -0,0 +1,69 @@
+public static class MatchRatingConstants
+{
+    public const float GOAL_SCORED = 0.5f;
+    public const float ASSIST = 0.25f;
+    public const float GOAL_SCORED_TEAM = 0.35f;
+    public const float GOAL_LOST_TEAM = -0.35f;
+
+    public const float COUNTER_BALL_LOSS = -0.35f;
+    public const float COUNTER_BALL_INTERCEPTION = 0.35f;
+    public const float COUNTER_PASS_FAIL = -0.3f;
+
+    public const float LONG_SAVE_SUCCESS = 0.1f;
+    public const float LONG_SAVE_FAIL = -0.25f;
+    public const float LONG_SHOT_SUCCESS = 0.2f;
+    public const float LONG_SHOT_FAIL = -0.05f;
+
+    public const float COUNTER_SAVE_SUCCESS = 0.2f;
+    public const float COUNTER_SAVE_FAIL = -0.2f;
+    public const float COUNTER_SAVE_CORNER = 0.15f;
+    public const float COUNTER_SHOT_SUCCESS = 0.15f;
+    public const float COUNTER_SHOT_FAIL = -0.15f;
+
+    public const float NORMAL_SAVE_SUCCESS = 0.3f;
+    public const float NORMAL_SAVE_FAIL = -0.15f;
+    public const float NORMAL_SAVE_CORNER = 0.2f;
+    public const float NORMAL_SHOT_SUCCESS = 0.1f;
+    public const float NORMAL_SHOT_FAIL = -0.2f;
+
+    public const float GOOD_SAVE_SUCCESS = 0.4f;
+    public const float GOOD_SAVE_FAIL = -0.1f;
+    public const float GOOD_SAVE_CORNER = 0.25f;
+    public const float GOOD_SHOT_SUCCESS = 0.1f;
+    public const float GOOD_SHOT_FAIL = -0.2f;
+
+    public const float ONE_ON_ONE_SAVE_SUCCESS = 0.5f;
+    public const float ONE_ON_ONE_SAVE_FAIL = -0.05f;
+    public const float ONE_ON_ONE_SAVE_CORNER = 0.3f;
+    public const float ONE_ON_ONE_SHOT_SUCCESS = 0.05f;
+    public const float ONE_ON_ONE_SHOT_FAIL = -0.45f;
+
+    public const float BASIC_PASS_SUCCESS = 0.1f;
+    public const float BASIC_PASS_FAIL = -0.1f;
+    public const float ADVANCED_PASS_SUCCESS = 0.15f;
+    public const float ADVANCED_PASS_FAIL = -0.05f;
+
+    public const float PENALTY_MISSED = -0.5f;
+    public const float PENALTY_SAVED = 0.5f;
+    public const float PENALTY_WON = 0.35f;
+
+    public const float CORNER_SUCCESS = 0.15f;
+    public const float CORNER_FAIL = -0.15f;
+    public const float CORNER_WON_POSITIONING = 0.25f;
+    public const float CORNER_LOST_POSITIONING = -0.25f;
+    public const float CORNER_WON_HEADER = 0.15f;
+    public const float CORNER_LOST_HEADER = -0.15f;
+
+    public const float DRIBBLE_DUEL_WON = 0.15f;
+    public const float DRIBBLE_DUEL_LOST = -0.15f;
+
+    public const float KEY_DRIBBLE_DUEL_WON = 0.25f;
+    public const float KEY_DRIBBLE_DUEL_LOST = -0.25f;
+
+    public const float CROSS_SUCCESS = 0.15f;
+    public const float CROSS_FAIL = -0.15f;
+    public const float CROSS_WON_POSITIONING = 0.2f;
+    public const float CROSS_LOST_POSITIONING = -0.2f;
+    public const float CROSS_WON_HEADER = 0.1f;
+    public const float CROSS_LOST_HEADER = -0.1f;
+}
